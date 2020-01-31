0398446057890163c7c9d62d5fecab136a4b2649
mikedilulo <mikedilulo2019@gmail.com>
2020-01-30 20:00:59 -0700

    Game loop input / Program.cs has .Run() method

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using ConsoleAdventure.Project;
+using ConsoleAdventure.Project.Controllers;
 
 namespace ConsoleAdventure
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-
-        }
+      //NOTE Need to have the controller in here with the .Run() method to keep the application running. This is where the application will be running from Program.cs. Need to implement Adventures.Project.Controllers
+      GameController gc = new GameController();
+      gc.Run();
     }
+  }
 }
